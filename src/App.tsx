f1083c6b55588b37e2769e6e0da9822f47a86422
ygcedu <937601471@qq.com>
2021-08-01 15:26:04 +0800

    删除无用文件，准备开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
-import Button from 'components/Button';
-import Box from 'components/Box';
 
 function App() {
   return (
-    <Box>
-      <Button>你好</Button>
-    </Box>
+    <div>Hello world</div>
   );
 }
 
