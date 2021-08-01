35e06617ea242324a263df283836530905fd74ca
ygcedu <937601471@qq.com>
2021-08-01 20:57:50 +0800

    搞定3个图标

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 // 使用 svg loader 后，x 变为一个对象而不是文件路径
 require('icons/money.svg');// TreeShaking 不适用于 require，此处生效
 require('icons/labels.svg');
+require('icons/statistics.svg');
 
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -15,7 +16,16 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.3333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -25,15 +35,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <svg fill="red" className="icon">
-            <use xlinkHref="#money"/>
+          <svg className="icon">
+            <use xlinkHref="#labels"/>
           </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money"/>
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#statistics"/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
