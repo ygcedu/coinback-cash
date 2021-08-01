5c649f5562e0f0dba1040583c9cd9acc5710be97
ygcedu <937601471@qq.com>
2021-08-01 15:14:25 +0800

    使用 styled-components 封装组件
    1. yarn add styled-components
    2. yarn add --dev @types/styled-components

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  color: red;
+  border: none;
+  background: green;
+
+  &:hover {
+    background: black;
+  }
+`;
+
+export default Button;
