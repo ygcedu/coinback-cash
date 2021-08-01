89a92b9e98544179b72c2d817703b79374c54c37
ygcedu <937601471@qq.com>
2021-08-01 16:27:46 +0800

    路由使用 hash 模式

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
