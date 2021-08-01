bfb678a15e4f804d0d375ffa9eed23bafedea282
ygcedu <937601471@qq.com>
2021-08-01 16:06:59 +0800

    CRM 法使用 react router
    1. yarn add react-router-dom
    2. yarn add @types/react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div>Hello world</div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/tags">
+            <Tags/>
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics/>
+          </Route>
+          <Redirect exact from="/" to="/money"/>
+          <Route path="*">
+            <NoMatch/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return (
+    <div>页面不存在，你丫输错地址了吧！</div>
+  );
+}
+
+function Statistics() {
+  return <h2>统计页</h2>;
+}
+
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页</h2>;
 }
 
 export default App;
