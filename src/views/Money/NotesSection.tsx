9e54d537942b2167b7ce813349a367a032bbb8a2
ygcedu <937601471@qq.com>
2021-08-02 23:37:07 +0800

    模块化 Money.tsx

---
+++ src/views/Money/NotesSection.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const NotesSection = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+
+  > label {
+    display: flex;
+    align-items: center;
+
+    > .x {
+      margin-right: 16px;
+      // 不换行
+      white-space: nowrap;
+    }
+
+    > input {
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+`;
+
+export {NotesSection};
