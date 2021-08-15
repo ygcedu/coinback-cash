e2c9155531c9b17f1bcaee93fc1cbb35c8018405
ygcedu <937601471@qq.com>
2021-08-15 19:38:40 +0800

    完成标签编辑页面顶部标题栏样式

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
-import {useTags} from 'useTags';
 import Layout from 'components/Layout';
 import Icon from 'components/Icon';
 import {Button} from 'components/Button';
+import styled from 'styled-components';
 
-type Params = {
-  id: string
-}
+// type Params = {
+//   id: string
+// }
+
+const Topbar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background: white;
+`;
 
 const Tag: React.FC = () => {
-  const {findTag} = useTags();
-  let {id} = useParams<Params>();
-  const tag = findTag(parseInt(id));
+  // const {findTag} = useTags();
+  // let {id} = useParams<Params>();
+  // const tag = findTag(parseInt(id));
+
   return (
     <Layout>
-      <header>
+      <Topbar>
         <Icon name="left"/>
         <span>编辑标签</span>
-      </header>
+        <span/>
+      </Topbar>
       <div>
         <label>
           <span>标签名</span>
