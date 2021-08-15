e6f0320cdef52fe0b9e095b041bc63776f51a003
ygcedu <937601471@qq.com>
2021-08-15 15:55:43 +0800

    重构：将tag:string 重构为 tag:{id:number;name:string}

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -15,7 +15,7 @@ type Category = ('-' | '+');
 
 function Money() {
   const [selected, setSelected] = useState({
-    tags: [] as string[],
+    tagIds: [] as number[],
     note: '',
     category: '-' as Category,
     amount: 0
@@ -27,16 +27,8 @@ function Money() {
   };
   return (
     <MyLayout>
-      {selected.tags.join(',')}
-      <hr/>
-      {selected.note}
-      <hr/>
-      {selected.category}
-      <hr/>
-      {selected.amount}
-      <hr/>
-      <TagsSection value={selected.tags}
-                   onChange={tags => onChange({tags})}/>
+      <TagsSection value={selected.tagIds}
+                   onChange={tagIds => onChange({tagIds})}/>
       <NoteSection value={selected.note}
                    onChange={note => onChange({note})}/>
       <CategorySection value={selected.category}
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -52,9 +52,9 @@ function Tags() {
     <Layout>
       <TagList>
         {tags.map(tag =>
-          <li key={tag}>
+          <li key={tag.id}>
             <Link to={'/tags/' + tag}>
-              <span className="oneLine">{tag}</span>
+              <span className="oneLine">{tag.name}</span>
               <Icon name="right"></Icon>
             </Link>
           </li>
