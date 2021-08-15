306c90c29c0b6536158dba11253ba5902f17eb4c
ygcedu <937601471@qq.com>
2021-08-15 12:29:25 +0800

    Tag 组件添加标签样式

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,13 +1,36 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import {useTags} from 'useTags';
+import styled from 'styled-components';
+import Icon from 'components/Icon';
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+
+  > li {
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px;
+    display: flex;
+    justify-content: space-between; // 左右分开
+    align-items: center; // 上下居中
+  }
+`;
 
 function Tags() {
-  const {tags, setTags} = useTags();
+  const {tags} = useTags();
 
   return (
     <Layout>
-      <h2>标签页</h2>
+      <TagList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <span>{tag}</span>
+            <Icon name="right"></Icon>
+          </li>
+        )}
+      </TagList>
     </Layout>
   );
 }
