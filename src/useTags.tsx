3147db52ae297194ec3a0fbfe328891df9ce74e7
ygcedu <937601471@qq.com>
2021-08-15 22:42:38 +0800

    另一种方式实现数据修改

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -21,25 +21,13 @@ const useTags = () => {
     }
     return result;
   };
-  const updateTag = (id: number, obj: { name: string }) => {
-    // 1. 获取你要改的 tag 的下标
-    const index = findTagIndex(id);
-    // vue：tags.splice(index: 1,{id: id,name: obj.name})
-    // 2. 深拷贝 tags 得到 tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    // 3. 把 tagsClone 的第 index 删掉，换成 {id:id, name: obj.name}
-    tagsClone.splice(index, 1, {id: id, name: obj.name});
-    // React 的 useState 发现如果传入的 tags 地址没有变，就不做任何处理
-    setTags(tagsClone); // 不可变数据 tag，newTags
+  const updateTag = (id: number, {name}: { name: string }) => {
+    // map() 不会改变原始数组
+    setTags(tags.map(tag => tag.id === id ? {id, name} : tag));
   };
   const deleteTag = (id: number) => {
-    // 获取你要删除的 tag 的下标
-    const index = findTagIndex(id);
-    // 2. 深拷贝 tags 得到 tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    // 3. 把 tagsClone 的第 index 删掉
-    tagsClone.splice(index, 1);
-    setTags(tagsClone);
+    // filter 会返回一个新数组
+    setTags(tags.filter(tag => tag.id !== id));
   };
   return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag};
 };
