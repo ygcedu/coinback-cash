2682c38d8bdb9a661b6329881ab8d74aa3044a4f
ygcedu <937601471@qq.com>
2021-08-17 00:05:15 +0800

    完成记录页 HTML 部分

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -21,6 +21,10 @@ const defaultFormData = {
   amount: 0
 };
 
+const CategoryWrapper = styled.div`
+  background: #c4c4c4;
+`;
+
 function Money() {
   const [selected, setSelected] = useState(defaultFormData);
 
@@ -44,8 +48,10 @@ function Money() {
                    onChange={tagIds => onChange({tagIds})}/>
       <NoteSection value={selected.note}
                    onChange={note => onChange({note})}/>
-      <CategorySection value={selected.category}
-                       onChange={category => onChange({category})}/>
+      <CategoryWrapper>
+        <CategorySection value={selected.category}
+                         onChange={category => onChange({category})}/>
+      </CategoryWrapper>
       <NumberPadSection value={selected.amount}
                         onChange={amount => onChange({amount})}
                         onOk={submit}/>
