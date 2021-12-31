fcb5c7f3b99efe0c79df20a7572c02cd4b4e79e8
ygcedu <937601471@qq.com>
2021-12-31 19:21:41 +0800

    增大 tag 起始值

--- src/lib/createId.tsx
+++ src/lib/createId.tsx
@@ -1,4 +1,4 @@
-let tagId = parseInt(window.localStorage.getItem('tagIdMax') || '0');
+let tagId = parseInt(window.localStorage.getItem('tagIdMax') || '100');
 let recordId = parseInt(window.localStorage.getItem('recordIdMax') || '0');
 
 const createId = (name: 'tag' | 'record'): number => {
